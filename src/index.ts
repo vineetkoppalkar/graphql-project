a4e9cdbd0353f87841fad10c2d5aad563b0e6bba
Vineet <koppalkarvineet@gmail.com>
2021-03-05 22:57:31 -0500

    Setup initial crud app with graphql

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata';
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+
+const main = async () => {
+  console.log('Connecting to db');
+  const orm = await MikroORM.init(microConfig);
+
+  console.log('Running migrations');
+  await orm.getMigrator().up();
+
+  console.log('Creating the Express server');
+  const app = express();
+
+  console.log('Creating the Apollo server');
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  console.log('Creating a GraphQL endpoint');
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000');
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
